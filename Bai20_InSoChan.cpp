041d65328ba7d25ca40e710bdfeb4c51a5ad75be
nguyenhoangkhoi06011 <nguyenhoangkhoi012021@gmail.com>
2024-11-25 16:35:22 +0700

    InSoChan

--- Bai20_InSoChan.cpp
+++ Bai20_InSoChan.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    int n;
-    cout << "Nhap so n: ";
-    cin >> n;
-    if (n % 2 == 0) {
-        cout << n << " la so chan" << endl;
-    }
-    else {
-        cout << n << " la so le" << endl;
-    }
-    return 0;
+int main()
+{
+	cout << "nhap so chan!\n";
+	int so, tongchan = 0, tongle = 0,tong=0;
+	cout << "nhap so:"; cin >> so;
+	for (int i = 1; i < so; i++)
+	{
+		tong += i;
+		if (i % 2 == 0)
+		{
+			cout << i << " ";
+			tongchan += i;
+		}
+		else
+		{
+			tongle += i;
+		}
+	}
+	cout << "\ntong cac so:" << tong;
+	cout << "\ntong cac so chan:" << tongchan;
+	cout << "\ntong cac so le:" << tongle;
+	cout << endl;
+	return 0;
 }
